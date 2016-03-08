5b3d0ffdc06e9971863c7ff478606111694b99d6
imanu01 <imanu01@users.noreply.github.com>
2016-03-08 12:31:40 +0100

    Update battleship1.py
    chnaged guess_ship function to return False or True (when player wins game).

--- battleship/battleship1.py
+++ battleship/battleship1.py
@@ -8,7 +8,7 @@ board = []
 class Board(object):
     
 	
-    def __init__( self, board, number, size, ):
+    def __init__( self, board, number, size ):
         self.board = []
         self.number = number
         self.size = size
@@ -19,8 +19,7 @@ class Board(object):
     def get_size( self ):
         return self.size
 
-    def get_board( self ):
-        return self.board
+    
 
 #function creates the position of the ship on board
     def change_board_square( self,row,col,contents ):
@@ -67,33 +66,34 @@ class Player( object ):
         guess_row = int( input( "%s Please guess a row number:" % ( playa.player_name ) ) )# receives guessed row number from player. {}.format(name)
         guess_col = int(input("Also guess a column number: "))# receives guessed column number from player
 
-        win = guess_row == ship_row and guess_col == ship_col
+        #win = (guess_row == ship_row and guess_col == ship_col)
 
         
         
             
                 
-            # conditions whereby the player looses
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4): # Case whereby player guesses out of range
-            print("Oops, %s, that's not even in the ocean." %(playa.player_name))
+       # conditions whereby the player looses
+        if ( guess_row < 0 or guess_row > 4 ) or ( guess_col < 0 or guess_col > 4 ): # Case whereby player guesses out of range
+            print( "Oops, %s, that's not even in the ocean." %( playa.player_name ))
             
-        elif (playa.player_board.board[guess_row][guess_col] == "X"): #Case where player repeats previous guess.
-            print("%s, You guessed that one already."%(playa.player_name))
-            
+        elif ( playa.player_board.board[ guess_row ][ guess_col ] == "X" ): #Case where player repeats previous guess.
+            print( "%s, You guessed that one already."% ( playa.player_name ) )
+            return False
 
             
         elif guess_row != ship_row or guess_col != ship_col: #case whereby player's guess is wrong and not one of the two cases above
-            print( "%s, You missed the battleship!"%(playa.player_name))
+            print( "%s, You missed the battleship!"% ( playa.player_name ) )
             playa.player_board.change_board_square( guess_row, guess_col, "X" )
             #player_board.board[guess_row][guess_col]= "X"
             #board.get_board()[guess_row][guess_col] = "X"
+            return False
         
         
          
         else:
             #if guess_row == ship_row and guess_col == ship_col: # Condition where player wins
-            print("Congratulations! You sunk the battleship!")
-            return 1
+            print( "Congratulations! You sunk the battleship!" )
+            return True
 
 """player_one = Player('Joe', board, 1, 5),
 player_one.number
@@ -101,7 +101,7 @@ player_one.player_board.show_board()
 
 
                 
-board_one = Board(board1, 1, 5 )
+board_one = Board( board1, 1, 5 )
 board_two = Board(board2, 2, 5 )
 
 board_one.show_board()
